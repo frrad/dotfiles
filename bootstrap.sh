f966972a2727a70a520fab10613981ff0fb7f228
Frederick Robinson <frederick.robinson@gmail.com>
2018-08-30 05:47:56 +0000

    only install deps if necessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,18 @@
 
 set -x
 
-apt-get update
-apt-get install -y puppet git
 target=`su -c 'echo $HOME/dotfiles' $SUDO_USER`
+
+if ! [ -x "$(command -v git)" ]; then
+  apt-get update
+  apt-get install -y git
+fi
+
+if ! [ -x "$(command -v puppet)" ]; then
+  apt-get update
+  apt-get install -y puppet
+fi
+
 if [ ! -d "$target" ]; then
   su -c "git clone https://github.com/frrad/dotfiles.git $target" $SUDO_USER
 fi
