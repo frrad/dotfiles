6f001c5861b8e28e27ea6c5c65b6a740f1ee4890
Frederick Robinson <frederick.robinson@gmail.com>
2020-04-03 17:20:58 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,5 +17,7 @@ fi
 if [ ! -d "$target" ]; then
   su -c "git clone https://github.com/frrad/dotfiles.git $target" $SUDO_USER
 fi
+
+cd ${target}/puppet
 r10k puppetfile install
 puppet apply --test $target/puppet/main.pp --modulepath=$target/puppet/modules
