a10c40a63ffd9fda3cee59b8f94a2967794dd375
Frederick Robinson <frederick.robinson@frrad.com>
2021-05-10 19:50:48 -0700

    Misc (#14)
    * python interpretter
    
    * easier to read font on dark screen
    
    * venv by default
    
    * misc

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -8,6 +8,7 @@
  '(package-selected-packages
    (quote
 	(auto-package-update smart-compile diminish counsel yaml-mode go-rename magit company-go py-autopep8 smex)))
+ '(python-shell-interpreter "python3")
  '(rcirc-default-nick "frrad")
  '(rcirc-server-alist (quote (("home.frrad.com" :nick "Frederick" nil nil))))
  '(sql-port 5439)
@@ -20,4 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(font-latex-sectioning-2-face ((t (:inherit font-latex-sectioning-3-face))))
  '(font-latex-sectioning-3-face ((t (:inherit font-latex-sectioning-4-face))))
- '(font-latex-sectioning-4-face ((t (:inherit font-latex-sectioning-5-face)))))
+ '(font-latex-sectioning-4-face ((t (:inherit font-latex-sectioning-5-face))))
+ '(font-lock-function-name-face ((t (:foreground "color-220"))))
+ '(font-lock-string-face ((t (:foreground "color-51"))))
+ '(magit-section-highlight ((t (:background "black")))))
